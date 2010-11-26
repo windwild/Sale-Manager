e54bffd9d03aa05acea1af2f8703c79f701f8e01
windwild <gjywindwild@gmail.com>
2010-11-26 23:16:19 +0800

    delete some bug from file show*.php

--- ShowInfo.php
+++ ShowInfo.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<!--
-Design by Free CSS Templates
-http://www.freecsstemplates.org
-Released for free under a Creative Commons Attribution 2.5 License
-
-Name       : Accomplishable
-Description: A two-column, fixed-width design.
-Version    : 1.0
-Released   : 20090731
-
--->
-<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="content-type" content="text/html; charset=gb2312" />
 <title></title>
--- ShowTop10.php
+++ ShowTop10.php
@@ -1,22 +1,3 @@
-<%@ page language="java" import="java.util.*,cn.edu.hit.*,java.sql.*" pageEncoding="gb2312"%>
-<%
-String path = request.getContextPath();
-String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
-%>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<!--
-Design by Free CSS Templates
-http://www.freecsstemplates.org
-Released for free under a Creative Commons Attribution 2.5 License
-
-Name       : Accomplishable
-Description: A two-column, fixed-width design.
-Version    : 1.0
-Released   : 20090731
-
--->
-<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="content-type" content="text/html; charset=gb2312" />
 <title></title>
